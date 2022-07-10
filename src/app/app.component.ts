ade4fc50e3befc8bbced85f162b9bf5ddb959af3
Stanisław Nieradko <stanislaw@nieradko.com>
2022-07-10 09:46:14 +0200

    feat: add Data Binding & Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-root',
   template: ` <div>
     <h1>{{ pageTitle }}</h1>
-    <p>My First Component</p>
+    <pm-products></pm-products>
   </div>`,
 })
 export class AppComponent {
