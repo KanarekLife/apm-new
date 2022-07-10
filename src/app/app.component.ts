9affc0cb90fd52bad9a234426b7840ad971138e6
Stanisław Nieradko <stanislaw@nieradko.com>
2022-07-10 09:13:27 +0200

    feat: update app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <div>
+    <h1>{{ pageTitle }}</h1>
+    <p>My First Component</p>
+  </div>`,
 })
 export class AppComponent {
-  title = 'apm-new';
+  pageTitle: string = 'Acme Product Management';
 }
