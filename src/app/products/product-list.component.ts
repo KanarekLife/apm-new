93e6a6660bca57a25efe65a94cfc61286acae03e
Stanisław Nieradko <stanislaw@nieradko.com>
2022-07-10 10:29:47 +0200

    feat: finished Services and Dependency Injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -12,6 +13,8 @@ export class ProductListComponent implements OnInit {
   imageMargin = 2;
   showImage = false;
 
+  constructor(private productService: ProductService) {}
+
   private _listFilter: string = '';
   get listFilter(): string {
     return this._listFilter;
@@ -21,40 +24,18 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.performFilter(value);
   }
 
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-  ];
-
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product) =>
       product.productName.toLocaleLowerCase().includes(filterBy)
     );
   }
-
-  filteredProducts: IProduct[] = this.products;
+  products: IProduct[] = [];
+  filteredProducts: IProduct[] = [];
 
   ngOnInit(): void {
-    console.log('On ngInit()');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
   toggleImage(): void {
     this.showImage = !this.showImage;
